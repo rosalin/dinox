a0680c8fc109c53ddcd776d04b39fb5a8d5d8a79
Eduardo <eduardo@eduardo-Aspire-E1-471.(none)>
2014-03-17 09:56:12 -0300

    commit

--- db/migrate/20140214155845_create_site_contacts.rb
+++ db/migrate/20140214155845_create_site_contacts.rb
@@ -1,6 +1,6 @@
 class CreateSiteContacts < ActiveRecord::Migration
   def change
-     create_table :contacts do |t|
+     create_table :site_contacts do |t|
       t.string :name
       t.string :email
       t.string :telefone
